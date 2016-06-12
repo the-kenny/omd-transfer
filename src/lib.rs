eeac6114641e5a77f20c7086221668c9e9dcde11
Moritz Ulrich <moritz@tarn-vedra.de>
2016-06-12 12:34:27 +0200

    get rid of unstable feature for boxes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(box_syntax, box_patterns)]
-
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate log;
 
--- src/wifi.rs
+++ src/wifi.rs
@@ -97,9 +97,12 @@ impl<'a> WifiNetwork<'a> {
 
     if let Ok(Array(props, _)) = p.get("Properties") {
       for prop in props {
-        if let DictEntry(box Str(prop_name), box Variant(box Str(prop_val))) = prop {
+        if let DictEntry(prop, val) = prop {
+          let prop_name: &str = prop.as_ref().inner().unwrap();
+          let prop_val:  &str =  val.as_ref().inner().unwrap();
+
           if prop_name == "ssid" {
-            let mut val = prop_val;
+            let mut val = prop_val.to_string();
             if val.starts_with("\"") { val.remove(0); }
             if val.ends_with("\"") {
               let len = val.len();
@@ -136,72 +139,5 @@ pub fn test_dbus() {
   let camera_network = interface.find_network(&network_name).unwrap();
 
   camera_network.associate();
-  original_network.associate();
+  // original_network.associate();
 }
-
-// fn associate_network<'a>(conn: &'a Connection,
-//                          interface: &Props<'a>,
-//                          network: &Path) -> Option<()> {
-
-//   Some(())
-// }
-
-
-// fn current_network<'a>(conn: &'a Connection, interface: Path) -> Option<Path<'a>> {
-
-// }
-
-// fn find_network<'a>(conn: &'a Connection,
-//                     interface: &Props<'a>,
-//                     network_name: &str) -> Option<Path<'a>> {
-//   use dbus::MessageItem::*;
-
-//   let network_name = format!("\"{}\"", network_name);
-
-//   if let Ok(Array(networks, _)) = interface.get("Networks") {
-//     for network in networks {
-//       if let MessageItem::ObjectPath(network) = network {
-//         let p = Props::new(conn,
-//                            "fi.w1.wpa_supplicant1",
-//                            network.clone(),
-//                            "fi.w1.wpa_supplicant1.Network",
-//                            100);
-//         if let Ok(Array(props, _)) = p.get("Properties") {
-//           for prop in props {
-//             if let DictEntry(box Str(prop_name), box Variant(box Str(prop_val))) = prop {
-//               println!("{}: {:?}", prop_name, prop_val);
-//               if prop_name == "ssid" && prop_val == network_name {
-//                 return Some(network)
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-
-//   None
-// }
-
-// fn find_interface<'a>(conn: &'a Connection, interface_name: &str) -> Option<Path<'a>> {
-//   let p = Props::new(conn,
-//                      "fi.w1.wpa_supplicant1",
-//                      "/fi/w1/wpa_supplicant1",
-//                      "fi.w1.wpa_supplicant1",
-//                      1000);
-
-//   if let Ok(MessageItem::Array(interfaces, _)) = p.get("Interfaces") {
-//     for path in interfaces {
-//       if let MessageItem::ObjectPath(path) = path {
-//         let ip = interface_props(conn, &path);
-//         if let Ok(MessageItem::Str(ifname)) = ip.get("Ifname") {
-//           if ifname == interface_name {
-//             return Some(path)
-//           }
-//         }
-//       }
-//     }
-//   }
-
-//   None
-// }
